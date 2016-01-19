3356f41ce944896f8809ef6e4164678c983b123a
Nicholas Lambert <nick@thedesigniac.com>
2016-01-18 20:40:59 -0700

    Changes to Program.cs

--- PracticeProjects/ClassPractice/Program.cs
+++ PracticeProjects/ClassPractice/Program.cs
@@ -2,11 +2,38 @@
 
 namespace ClassPractice
 {
-	class MainClass
-	{
-		public static void Main (string[] args)
-		{
-			Console.WriteLine ("Hello World!");
+	class Person {
+		private string name, birthdate;
+		private int day, month, year, age;
+
+		public Person(string newName, int newAge) {
+			name = newName;
+			age = newAge;
 		}
-	}
+
+		public void setBirthdate(int newDay, int newMonth, int newYear) {
+			day = newDay;
+			month = newMonth;
+			year = newYear;
+			birthdate = (day + "/" + month + "/" + year);
+		}
+
+		public string getBirthdate() {
+			return birthdate;
+		}
+
+		public string getName() {
+			return name;
+		}
+	}//end person
+
+	class Program {
+		public static void main() {
+			Person person1 = new Person ("Nicholas", 20);
+			person1.setBirthdate (8, 7, 1995);
+
+			Console.WriteLine (person1.getName ());
+			Console.WriteLine(person1.getBirthdate ());
+		}
+	}//end program
 }
